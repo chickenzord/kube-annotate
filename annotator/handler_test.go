f83d7686ec2b72bb17e67d880197589ea04db2f2
Akhyar Amarullah <akhyrul@gmail.com>
2018-12-21 01:24:44 +0700

    add mutate handler tests

--- annotator/handler_test.go
+++ annotator/handler_test.go
@@ -1,6 +1,7 @@
 package annotator
 
 import (
+	"bytes"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -8,7 +9,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRulesHandlers(t *testing.T) {
+func TestRulesHandler(t *testing.T) {
 	req, err := http.NewRequest("GET", "/rules", nil)
 	if err != nil {
 		t.Fatal(err)
@@ -20,4 +21,32 @@ func TestRulesHandlers(t *testing.T) {
 
 	assert.Equal(t, http.StatusOK, rr.Code)
 	assert.Equal(t, "application/json", rr.HeaderMap.Get("Content-Type"))
+}
+
+func TestMutateHandlerEmptyBody(t *testing.T) {
+	req, err := http.NewRequest("POST", "/mutate", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(MutateHandler)
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusBadRequest, rr.Code)
+}
+
+func TestMutateHandlerWrongContentType(t *testing.T) {
+	body := bytes.NewBufferString("{}")
+	req, err := http.NewRequest("POST", "/mutate", body)
+	req.Header.Set("Content-Type", "text/plain")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(MutateHandler)
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusBadRequest, rr.Code)
 }
